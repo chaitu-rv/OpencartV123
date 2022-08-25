5c5b6fd4a6828de4320a9af3b658a33426e56573
chaitu <ragannagarichaitanya1@gmail.com>
2022-08-24 21:37:44 -0600

    added all testcases

--- src/test/java/pageObjects/HomePage.java
+++ src/test/java/pageObjects/HomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends BasePage {
+
 	public HomePage(WebDriver driver) {
 		super(driver);
 	}
@@ -15,11 +16,18 @@ public class HomePage extends BasePage {
 
 	@FindBy(linkText = "Register")
 	WebElement lnkRegister;
-	
+
 	@FindBy(linkText = "Login")   // Login link added in step6
 	WebElement linkLogin;
 	
-
+	@FindBy(xpath="//input[@placeholder='Search']")  //For Search Product Test
+	WebElement txtSearchbox;
+	
+	@FindBy(xpath="//div[@id='search']//button[@type='button']") //For Search Product Test
+	WebElement btnSearch;
+	
+	
+		
 	// Action Methods
 	public void clickMyAccount() {
 		lnkMyaccount.click();
@@ -33,5 +41,17 @@ public class HomePage extends BasePage {
 	{
 		linkLogin.click();
 	}
+	
+	public void enterProductName(String pName)   //For Search Product Test
+	{
+		txtSearchbox.sendKeys(pName);
+	}
+	
+	public void clickSearch()  //For Search Product Test
+	{
+		btnSearch.click();
+	}
+	
+	
 	
 }
